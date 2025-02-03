b961cc4af8af8f101d6ea01a21a8e6a8fb1e1902
minhngucode <caoscute2805@gmail.com>
2025-02-03 13:49:07 +0700

    gg

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './Components/Navbar';
+import Menu from './Components/Menu';
+import Booking from './Components/Booking';
+import Header from './Components/Header';
+
+
+function App() {
+  return (
+    <div style={{ backgroundColor: '#333333', minHeight: '100vh', color: 'white' }}>
+      <Navbar />
+      <Header />
+      <Menu />
+      <Booking />
+    </div>
+  );
+}
+
+export default App;
